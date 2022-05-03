270d4a0b3d057fef7641447ba96a73ae727aaca5
Esh shatabdi <eshshatabdi@gmail.com>
2022-05-02 23:46:48 -0400

    inventoryid set

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,7 +14,35 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('wareHouse').collection('service');
 
+
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+
+        });
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await serviceCollection.findOne(query);
+            res.send(item);
+        });
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 

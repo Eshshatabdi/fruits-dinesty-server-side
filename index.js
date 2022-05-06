97836d6c12bd5f37e3e9cd5a16b59c8b9a14f997
Esh shatabdi <eshshatabdi@gmail.com>
2022-05-06 17:05:38 -0400

    token set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -7,6 +8,27 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+function varifyingJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorizes access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        // console.log('decoded', decoded);
+        req.decoded = decoded;
+    })
+
+    next()
+
+
+}
 
 
 
@@ -20,7 +42,15 @@ async function run() {
         const serviceCollection = client.db('wareHouse').collection('service');
 
 
+        app.post('/login', async (req, res) => {
 
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+
+        })
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -70,12 +100,21 @@ async function run() {
             res.send(result);
 
         })
-        app.get('/services', async (req, res) => {
-            const email = req.query.email
+        app.get('/services', varifyingJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
 
-            const query = { supplierEmail: email };
-            const result = await serviceCollection.find(query).toArray()
-            res.send(result)
+            const email = req.query.email
+            if (email === decodedEmail) {
+                const query = { supplierEmail: email };
+                const result = await serviceCollection.find(query).toArray()
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+
+
+
 
         })
 
